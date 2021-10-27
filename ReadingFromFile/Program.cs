d93aae1834cf932fa11389ddc208ffb5667e09c3
Opilane TTHK <opilane@tthk.ee>
2021-10-27 11:31:56 +0300

    add read from file

---
+++ ReadingFromFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples\wishList";
+            string userFile = "wishlist";
+            string fullFilePath = $@"{rootDirectory}\{userFile}.txt";
+
+            string[] dataFromFile = File.ReadAllLines(fullFilePath);
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+
+            dataFromFile[0] = "garbage";
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+
+            File.WriteAllLines(fullFilePath, dataFromFile);
+
+        }
+    }
+}
